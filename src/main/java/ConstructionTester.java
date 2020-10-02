e205ea02b12e8a4e208605c7f9b5305d463c3e41
Israel Zewdie <israel.225610@nv.ccsd.net>
2020-10-02 07:19:26 +0000

    worked on both constructorTester and How Far

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -1,17 +1,39 @@
 import java.util.Scanner;
-
+ 
 public class ConstructionTester
 {
-    public static void main(String[] args)
-    {
-        double taxRate;
-        int windows;
-        int lumber;
-        double total;
-
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter the sales tax rate: ");
-        
-    }
+   public static void main(String[] args)
+   {
+       double taxRate;
+       int windows;
+       int lumber;
+       double total;
+ 
+       Scanner scanner = new Scanner(System.in);
+ 
+       System.out.println("Enter the sales tax rate: ");
+       taxRate = scanner.nextDouble();
+ 
+       System.out.println("How many boards do you need? ");
+       windows = scanner.nextInt();
+ 
+       System.out.println("How many windows do you need? ");
+       lumber = scanner.nextInt();
+ 
+ 
+       Construction build = new Construction(8.0, 11.0, taxRate);
+ 
+       total = build.lumberCost(lumber) + build.windowsCost(windows);
+       grandTotal = build.grandTotal(135.0);
+ 
+       System.out.println(total);
+       System.out.print(grandTotal);
+ 
+      
+      
+ 
+    
+      
+   }
 }
+ 
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -1,9 +1,29 @@
 import java.util.Scanner;
-
-public class HowFarAway 
+ 
+public class HowFarAway
 {
-    public static void main(String[] args)
-    {
-        // your code here.
-    }
+   public static void main(String[] args)
+   {
+       double startLatitude;
+       double startLongitude;
+       double endLatitude;
+       double endLongitude;
+       double distance;
+ 
+       Scanner scanner = new Scanner(System.in);
+ 
+       System.out.println("Enter the latitude of the starting location: ");
+       startLatitude = scanner.nextDouble();
+ 
+       System.out.println("Enter the longitude of the starting location: ");
+       startLongitude = scanner.nextDouble();
+ 
+       System.out.println("Enter the latitude of the ending location: ");
+       endLatitude = scanner.nextDouble();
+ 
+       System.out.println("Enter the longitude of the ending location: ");
+       endLongitude = scanner.nextDouble();
+ 
+ 
+   }
 }
